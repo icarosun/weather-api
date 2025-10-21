9ae9b4db4caec564137302c7d934c10b12dd1def
ícarosun <icaropereirasun@gmail.com>
2025-10-21 13:50:09 -0400

    Implement a new endpoint to aggregate station and observed hydrological data

---
+++ src/http/controllers/hydrological-data/aggregate/station-hydrological-data.ts
@@ -0,0 +1,33 @@
+import { StationRepositoryImpl } from '@/repositories/prisma/station-repository-impl'
+import { ElevationClimatologyRepositoryImpl } from '@/repositories/prisma/elevation-climatology-repository-impl'
+import { ObservedHydrologicalDataRepositoryImpl } from '@/repositories/prisma/observed-hydrological-data-repository-impl'
+import { GetLastObservedHydrologicalDataUseCase } from '@/use-cases/get-last-observed-hydrological-data'
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function stationObservedHydrologicalDataController(
+  request: FastifyReply,
+  reply: FastifyReply
+) {
+  const stationRepositoryImpl = new StationRepositoryImpl()
+
+  const stations = await stationRepositoryImpl.getAll();
+
+  const getObservedHydrologicalDataUseCase = 
+    new GetLastObservedHydrologicalDataUseCase(
+      new ObservedHydrologicalDataRepositoryImpl(),
+      new ElevationClimatologyRepositoryImpl()
+    );
+
+  const response = []
+
+  for (var station of stations) {
+    var observedHydrologicalData = await getObservedHydrologicalDataUseCase.execute({ stationId: station.id })
+
+    response.push({
+      ...station,
+      ...observedHydrologicalData
+    })
+  }
+
+  return reply.status(200).send(response)
+}
