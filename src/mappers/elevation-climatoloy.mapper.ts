a97d8a32904a9a71da3aa6b3982b12d0f5bdd0b3
Ícarosun <icaropereirasun@gmail.com>
2026-02-07 13:03:00 -0400

    Create mapper elevationClimatology

---
+++ src/mappers/elevation-climatoloy.mapper.ts
@@ -0,0 +1,9 @@
+export interface ElevationClimatologyDTO {
+  percentile_between_5_and_0: number,
+  percentile_between_10_and_5: number,
+  percentile_between_15_and_10: number,
+  percentile_between_85_and_90: number,
+  percentile_between_90_and_95: number,
+  percentile_between_95_and_100: number
+}
+
