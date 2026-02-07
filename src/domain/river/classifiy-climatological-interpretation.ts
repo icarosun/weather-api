607348eabf9c4f608a74961499746902737a7346
Ícarosun <icaropereirasun@gmail.com>
2026-02-07 13:00:43 -0400

    refactor: move rule to domain

---
+++ src/domain/river/classifiy-climatological-interpretation.ts
@@ -0,0 +1,58 @@
+import { ElevationClimatologyDTO } from "@/mappers/elevation-climatoloy.mapper"
+
+export enum ClimatologyInterpretation {
+  EMERGENCIA_ESTIAGEM = -3,
+  ALERTA_ESTIAGEM = -2,
+  ATENCAO_ESTIAGEM = -1,
+  NORMALIDADE = 0,
+  ATENCAO_INUNDACAO = 1,
+  ALERTA_INUNDACAO = 2,
+  EMERGENCIA_INUNDACAO = 3
+}
+
+export type ClassifyClimatologicalInterpretationProps = {
+  elevation: number,
+  climatologicalValues: ElevationClimatologyDTO
+}
+
+export function ClassifyClimatologicalInterpretation(
+  { elevation, climatologicalValues }: ClassifyClimatologicalInterpretationProps
+): ClimatologyInterpretation {
+  let interpretation: ClimatologyInterpretation = 0;
+
+  if (elevation < climatologicalValues!.percentile_between_95_and_100) {
+    interpretation = -3
+  } else if (
+    elevation >= climatologicalValues!.percentile_between_95_and_100 &&
+    elevation < climatologicalValues!.percentile_between_90_and_95
+  ) {
+    interpretation = -2
+  } else if (
+    elevation >= climatologicalValues!.percentile_between_90_and_95 &&
+    elevation < climatologicalValues!.percentile_between_85_and_90
+  ) {
+    interpretation = -1
+  } else if (
+    elevation >= climatologicalValues!.percentile_between_85_and_90 &&
+    elevation < climatologicalValues!.percentile_between_15_and_10
+  ) {
+    interpretation = 0
+  } else if (
+    elevation >= climatologicalValues!.percentile_between_15_and_10 &&
+    elevation < climatologicalValues!.percentile_between_10_and_5
+  ) {
+    interpretation = 1
+  } else if (
+    elevation >= climatologicalValues!.percentile_between_10_and_5 &&
+    elevation < climatologicalValues!.percentile_between_5_and_0
+  ) {
+    interpretation = 2
+  } else if (
+    elevation >= climatologicalValues!.percentile_between_5_and_0
+  ) {
+    interpretation = 3
+  }
+
+  return interpretation
+}
+
