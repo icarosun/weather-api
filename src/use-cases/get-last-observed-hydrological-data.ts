21c8d1165ae609a54c9b28dc1b9fe468a7fc45f9
Ícarosun <icaropereirasun@gmail.com>
2026-02-09 20:11:56 -0400

    refactor: extracted rules

--- src/use-cases/get-last-observed-hydrological-data.ts
+++ src/use-cases/get-last-observed-hydrological-data.ts
@@ -1,8 +1,9 @@
 import { ElevationClimatologyRepository } from '@/repositories/elevation-climatology-repository'
 import { ObservedHydrologicalDataRepository } from '@/repositories/observed-hydrological-data-repository'
-import { getClimatologicalInterpretation } from '@/utils/get-climatological-interpretation'
-import { getDayOfYear } from '@/utils/get-day-of-year'
-import { getDifferenceDatein24h } from '@/utils/get-difference-date-in-24h'
+import { getDayOfYear, getDifferenceDatein24h } from '@/domain/river/date'
+import { ElevationClimatologyDTO } from '@/mappers/elevation-climatoloy.mapper'
+import { CalculateLevelRiverChange } from '@/domain/river/calculate-river-level-change'
+import { ClassifyClimatologicalInterpretation } from '@/domain/river/classifiy-climatological-interpretation'
 
 type GetLastObservedHydrologicalDataUseCaseRequest = {
   stationId: string
@@ -15,18 +16,19 @@ type GetLastObservedHydrologicalDataUseCaseResponse = {
   flow: number
   accumulated_rain: number
   station_id: string
-  climatologicalInterpretation: string
+  climatologicalInterpretation: number
+  dailyVariation: number | null
 }
 
 export class GetLastObservedHydrologicalDataUseCase {
   constructor(
     private observedHydrologicalDataRepository: ObservedHydrologicalDataRepository,
     private elevationClimatologyRepository: ElevationClimatologyRepository
-  ) {}
+  ) { }
 
   async execute({
     stationId,
-  }: GetLastObservedHydrologicalDataUseCaseRequest): Promise<GetLastObservedHydrologicalDataUseCaseResponse|null> {
+  }: GetLastObservedHydrologicalDataUseCaseRequest): Promise<GetLastObservedHydrologicalDataUseCaseResponse | null> {
     const observedHydrologicalData =
       await this.observedHydrologicalDataRepository.getLast(stationId)
 
@@ -34,26 +36,40 @@ export class GetLastObservedHydrologicalDataUseCase {
       return null
     }
 
+    const date24hChange = getDifferenceDatein24h(observedHydrologicalData!.date)
+
+    const previousDayObservedHydrologicalData =
+      await this.observedHydrologicalDataRepository.getDataByDate(stationId, date24hChange)
+
+    const calculateDailyVariation: number | null = CalculateLevelRiverChange({
+      currentElevation: observedHydrologicalData?.elevation?.toNumber() ?? null,
+      previousElevation: previousDayObservedHydrologicalData?.elevation?.toNumber() ?? null
+    });
+
     const climatologicalRegister =
       await this.elevationClimatologyRepository.findByDayAndStation({
         day: getDayOfYear(observedHydrologicalData!.date),
         stationId,
       })
 
-    const climatologicalInterpretation = getClimatologicalInterpretation({
-      climatologicalRegister: climatologicalRegister!,
-      elevation: observedHydrologicalData!.elevation,
-    })
-
-    const date24hChange = getDifferenceDatein24h(observedHydrologicalData!.date)
-
-    const previousDayObservedHydrologicalData = 
-      await this.observedHydrologicalDataRepository.getPreviousData(stationId, date24hChange)
-
-    let calculateDailyVariation = null
-    if (previousDayObservedHydrologicalData) {
-      calculateDailyVariation = observedHydrologicalData!.elevation - previousDayObservedHydrologicalData!.elevation
+    if (!climatologicalRegister) {
+      return null;
     }
+
+    const elevationClimatologyDTO: ElevationClimatologyDTO = {
+      percentile_between_5_and_0: climatologicalRegister?.percentile_between_5_and_0?.toNumber(),
+      percentile_between_10_and_5: climatologicalRegister?.percentile_between_10_and_5?.toNumber(),
+      percentile_between_15_and_10: climatologicalRegister?.percentile_between_15_and_10?.toNumber(),
+      percentile_between_85_and_90: climatologicalRegister?.percentile_between_85_and_90?.toNumber(),
+      percentile_between_90_and_95: climatologicalRegister?.percentile_between_90_and_95?.toNumber(),
+      percentile_between_95_and_100: climatologicalRegister?.percentile_between_95_and_100?.toNumber()
+    }
+
+
+    const climatologicalInterpretation = ClassifyClimatologicalInterpretation({
+      elevation: observedHydrologicalData?.elevation.toNumber(),
+      climatologicalValues: elevationClimatologyDTO
+    })
 
     return {
       id: observedHydrologicalData!.id,
